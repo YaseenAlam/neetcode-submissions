4dcb932e70095bbb97bb56e80c8bb14acce87eb2
YaseenAlam <58832549+YaseenAlam@users.noreply.github.com>
2026-09-14 13:51:14 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map1 = {}
+        for s in strs:
+            key = ''.join(sorted(s))
+            if key not in map1:
+                map1[key] = []
+            map1[key].append(s)
+
+        return list(map1.values())
